d1540564cab48382387b373e9ef0237d71cfc8c9
Tina Lee <tina11503577@gmail.com>
2023-08-23 15:01:59 +0800

    [Test] - update BookingVo

--- src/main/java/feature/booking/vo/BookingVo.java
+++ src/main/java/feature/booking/vo/BookingVo.java
@@ -1,4 +1,30 @@
 package feature.booking.vo;
 
-public class BookingVo {
+import lombok.*;
+
+import javax.persistence.*;
+import java.io.Serial;
+import java.io.Serializable;
+import java.sql.Date;
+@Entity
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "booking")
+public class BookingVo implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "BOOKING_NO",insertable = false )
+    private Integer bookingno;
+    @Column(name = "MEM_NO" )
+    private Integer memno;
+    @Column(name = "BOOKING_STATE")
+    private  Integer bookingstate;
+    @Column(name = "BOOKING_DATE", insertable = false)
+    private Date bookingdate;
+
 }
