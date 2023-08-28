f65bf95987d0dcee1dbfb7b175cc813e4a240488
yoooche <che43700@gmail.com>
2023-08-29 02:29:38 +0800

    [up to date] - insert a bidding list and tried to make the records show on page when new participates onload the page but failed

--- src/main/java/feature/bid/controller/BidOneWebsocket.java
+++ src/main/java/feature/bid/controller/BidOneWebsocket.java
@@ -1,6 +1,10 @@
 package feature.bid.controller;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.Tuple;
 
 import javax.websocket.*;
 import javax.websocket.server.PathParam;
@@ -10,16 +14,29 @@ import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
+import static core.util.Constants.GSON;
+
 @ServerEndpoint("/BidOnePage/{memName}")
 public class BidOneWebsocket {
     private static final Set<Session> bidders = Collections.synchronizedSet(new HashSet<>());
-    private static Jedis jedis = new Jedis("localhost", 6379);
+    Jedis jedis = new Jedis("localhost", 6379);
 
     @OnOpen
     public void onOpen(@PathParam("memName") String memName, Session memSession) throws IOException {
         bidders.add(memSession);
         String text = String.format("Session ID = %s, connected; memName = %s", memSession.getId(), memName);
         System.out.println(text);
+
+        // 測試剛進入競標頁面的使用者也可以看到先前的出價紀錄
+//        Set<Tuple> allRecords = jedis.zrangeWithScores("1", 0, -1);
+//        for(Tuple record : allRecords){
+//            String bidder = record.getElement();
+//            Integer biddingRange = (int)record.getScore();
+//            JsonObject jsonObject = new JsonObject();
+//            jsonObject.addProperty("bidder", bidder);
+//            jsonObject.addProperty("biddingRange", biddingRange);
+//            memSession.getAsyncRemote().sendText(jsonObject.toString());
+//        }
     }
     @OnMessage
     public void onMessage(Session memSession, String message) {
@@ -27,9 +44,11 @@ public class BidOneWebsocket {
             if (session.isOpen())
                 session.getAsyncRemote().sendText(message);
         }
-        System.out.println("Message received:" + message);
-        jedis.select(1);
-        jedis.set("bidEvent1", message);
+        JsonObject jsonObject = GSON.fromJson(message, JsonObject.class);
+        String bidder = jsonObject.get("bidder").getAsString();
+        Integer biddingRange = jsonObject.get("biddingRange").getAsInt();
+//        System.out.println("Message received:" + message);
+        jedis.zadd("1", biddingRange, bidder); //key值不能寫死
     }
     @OnClose
     public void onClose(Session memSession, CloseReason reason) {
