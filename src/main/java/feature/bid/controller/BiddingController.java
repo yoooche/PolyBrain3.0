cdf16c72340656468e4bacaefc57e3097ec3fef3
yoooche <che43700@gmail.com>
2023-09-03 02:25:16 +0800

    [up to date] - bidding button can change the value by DB, timer also.

--- src/main/java/feature/bid/controller/BiddingController.java
+++ src/main/java/feature/bid/controller/BiddingController.java
@@ -1,7 +1,9 @@
 package feature.bid.controller;
 
+import com.google.gson.Gson;
 import feature.bid.service.BiddingService;
 import feature.bid.service.BiddingServiceImpl;
+import feature.bid.vo.BidEventVo;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,6 +11,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @WebServlet("/test")
 public class BiddingController extends HttpServlet {
@@ -26,6 +32,36 @@ public class BiddingController extends HttpServlet {
             biddingService = new BiddingServiceImpl();
             Integer bidEventNo = Integer.valueOf(bidEventId);
             biddingService.createOneOrder(bidEventNo);
+        }
+        if("rangeBarSetting".equals((message))){
+            biddingService = new BiddingServiceImpl();
+            Integer bidEventNo = Integer.valueOf(bidEventId);
+            BidEventVo bidEventVo =  biddingService.getEventByNo(bidEventNo);
+
+            Integer floorPrice =  bidEventVo.getFloorPrice();
+            Integer directivePrice =  bidEventVo.getDirectivePrice();
+
+            Map<String, Integer> priceRange = new HashMap<>();
+            priceRange.put("floorPrice", floorPrice);
+            priceRange.put("directivePrice", directivePrice);
+
+            resp.setContentType("application/json");
+            Gson gson = new Gson();
+            String jsonPrice = gson.toJson(priceRange);
+            PrintWriter out = resp.getWriter();
+            out.print(jsonPrice);
+            out.flush();
+
+        }
+        if("getBiddingTimer".equals(message)){
+            biddingService = new BiddingServiceImpl();
+            Integer bidEventNo = Integer.valueOf(bidEventId);
+            Map<String, String> timer = biddingService.getStartTimeByNo(bidEventNo);
+            Gson gson = new Gson();
+            String jsonTimer = gson.toJson(timer);
+            PrintWriter out = resp.getWriter();
+            out.print(jsonTimer);
+            out.flush();
         }
     }
 
--- src/main/java/feature/bid/service/BiddingServiceImpl.java
+++ src/main/java/feature/bid/service/BiddingServiceImpl.java
@@ -10,8 +10,8 @@ import feature.mem.vo.MemVo;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.Tuple;
 
-import java.util.List;
-import java.util.Set;
+import java.text.SimpleDateFormat;
+import java.util.*;
 import java.util.stream.Stream;
 
 public class BiddingServiceImpl implements BiddingService{
@@ -63,6 +63,22 @@ public class BiddingServiceImpl implements BiddingService{
     }
 
     @Override
+    public Map<String, String> getStartTimeByNo(Integer bidEventNo) {
+        BidEventVo bidEventVo = bidEventDao.selectById(bidEventNo);
+        Date startTimeOrigin = bidEventVo.getStartTime();
+        Date closeTimeOrigin = bidEventVo.getCloseTime();
+
+        SimpleDateFormat timerTrans = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String startTime = timerTrans.format(startTimeOrigin);
+        String closeTime = timerTrans.format(closeTimeOrigin);
+
+        Map<String, String> timer = new HashMap<>();
+        timer.put("startTime", startTime);
+        timer.put("closeTime", closeTime);
+        return timer;
+    }
+
+    @Override
     public void createOneOrder(Integer bidEventNo) {
         Set<Tuple> highestRecord =  jedis.zrevrangeWithScores(String.valueOf(bidEventNo), 0, 0);
         Stream<Tuple> tupleStream = highestRecord.stream();
@@ -70,6 +86,10 @@ public class BiddingServiceImpl implements BiddingService{
             String member = tuple.getElement();
             int score = (int)tuple.getScore();
 //            System.out.println("Member: " + member + ", Score: " + score); // 測試用
+//            String message = "Hi" + member + "! 您好，您以" +  score + "元的高價得標，請到訂單確認頁面完成後續的出貨流程。";
+//            jedis.select(1);
+//            jedis.set("bidOrder" + bidEventNo, message);
+
             MemVo memVo = memDao.selectByMemName(member);
             System.out.println(memVo); // 測試用
             bidOrderVo.setBidEventNo(bidEventNo); // 競標活動編號
