2655a2e4f0757f00309b2293d6f37ee8c43aec98
ps100310 <ps66391@gmail.com>
2023-09-06 15:39:13 +0800

    commit

--- src/main/java/feature/bookingtast/controller/Updateservlet.java
+++ src/main/java/feature/bookingtast/controller/Updateservlet.java
@@ -1,9 +1,8 @@
 package feature.bookingtast.controller;
 
-import feature.bookingtast.dao.TablebookingDAO;
 import feature.bookingtast.service.TablebookingService;
 import feature.bookingtast.vo.TablebookingVO;
-import com.google.gson.Gson;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
@@ -12,14 +11,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
 @WebServlet("/bookingtast/inserservlet")
 @MultipartConfig   //有用到檔案上傳的接收入口設置
-public class inserservlet extends HttpServlet {
+public class Updateservlet extends HttpServlet {
     public void doGet(HttpServletRequest req, HttpServletResponse res) throws
             ServletException, IOException {
         doPost(req, res);
@@ -68,23 +65,24 @@ public class inserservlet extends HttpServlet {
             java.sql.Date TABLE_DATE = java.sql.Date.valueOf(req.getParameter("TABLE_DATE").trim());
 
             Integer TABLE_NO = Integer.valueOf(req.getParameter("TABLE_NO").trim());
+
+
             Integer TABLE_MOR = Integer.valueOf(req.getParameter("TABLE_MOR").trim());
             Integer TABLE_EVE = Integer.valueOf(req.getParameter("TABLE_EVE").trim());
             Integer TABLE_NIGHT = Integer.valueOf(req.getParameter("TABLE_NIGHT").trim());
-
 
             /***************************2.開始修改資料*****************************************/
             TablebookingService tabSvc = new TablebookingService();
             TablebookingVO tablebookingVO = tabSvc.updatetab(TABLE_BOOK_NO, TABLE_DATE, TABLE_NO, TABLE_MOR, TABLE_EVE, TABLE_NIGHT);
 
             /***************************3.修改完成,準備轉交(Send the Success view)*************/
-            String url = "/view/bookingtast/1.jsp";
-            RequestDispatcher successView = req.getRequestDispatcher(url);// 成功轉交 update_emp_input.jsp
+            String url = "/view/bookingtast/2.jsp";
+            req.setAttribute("tablebookingVO", tablebookingVO);
+            RequestDispatcher successView = req.getRequestDispatcher(url); // 修改成功後,轉交listOneEmp.jsp
             successView.forward(req, res);
-            req.setAttribute("message", "修改成功");
 
 
         }
 
-}
+    }
 }
